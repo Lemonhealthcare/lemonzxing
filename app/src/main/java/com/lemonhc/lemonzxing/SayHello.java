433bb14694b71e745f41674e7c5cbdc32e1ae437
강성희 <sunghee.kang@lemonhc.com>
2020-03-26 11:59:56 +0900

    upload test class

---
+++ app/src/main/java/com/lemonhc/lemonzxing/SayHello.java
@@ -0,0 +1,15 @@
+package com.lemonhc.lemonzxing;
+
+import android.util.Log;
+
+/**
+ * Created by SH Kang on 2020/03/26.
+ * Copyright © 2020 LemonHealthcare. All rights reserved.
+ */
+public class SayHello {
+
+
+    public void print(){
+        Log.d("test@", "hello lemon world");
+    }
+}
